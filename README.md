bed5dff130b7e93d316b0e64ee544771645d62c8
fuzhengwei <184172133@qq.com>
2020-08-16 21:59:33 +0800

    小傅哥，文章代码更新。晚上好!
    面经手册 · 第4篇《HashMap数据插入、查找、删除、遍历，源码分析》

--- README.md
+++ README.md
@@ -12,4 +12,4 @@
 2. [面经手册 · 第1篇《认知自己的技术栈盲区》](https://bugstack.cn/interview/2020/07/30/%E9%9D%A2%E7%BB%8F%E6%89%8B%E5%86%8C-%E7%AC%AC1%E7%AF%87-%E8%AE%A4%E7%9F%A5%E8%87%AA%E5%B7%B1%E7%9A%84%E6%8A%80%E6%9C%AF%E6%A0%88%E7%9B%B2%E5%8C%BA.html)
 3. [面经手册 · 第2篇《数据结构，HashCode为什么使用31作为乘数？》](https://bugstack.cn/interview/2020/08/04/%E9%9D%A2%E7%BB%8F%E6%89%8B%E5%86%8C-%E7%AC%AC2%E7%AF%87-%E6%95%B0%E6%8D%AE%E7%BB%93%E6%9E%84-HashCode%E4%B8%BA%E4%BB%80%E4%B9%88%E4%BD%BF%E7%94%A831%E4%BD%9C%E4%B8%BA%E4%B9%98%E6%95%B0.html)
 4. [面经手册 · 第3篇《HashMap核心知识，扰动函数、负载因子、扩容链表拆分，深度学习》](https://bugstack.cn/interview/2020/08/07/%E9%9D%A2%E7%BB%8F%E6%89%8B%E5%86%8C-%E7%AC%AC3%E7%AF%87-HashMap%E6%A0%B8%E5%BF%83%E7%9F%A5%E8%AF%86-%E6%89%B0%E5%8A%A8%E5%87%BD%E6%95%B0-%E8%B4%9F%E8%BD%BD%E5%9B%A0%E5%AD%90-%E6%89%A9%E5%AE%B9%E9%93%BE%E8%A1%A8%E6%8B%86%E5%88%86-%E6%B7%B1%E5%BA%A6%E5%AD%A6%E4%B9%A0.html)
-
+5. [面经手册 · 第4篇《HashMap数据插入、查找、删除、遍历，源码分析》](https://bugstack.cn/interview/2020/08/13/%E9%9D%A2%E7%BB%8F%E6%89%8B%E5%86%8C-%E7%AC%AC4%E7%AF%87-HashMap%E6%95%B0%E6%8D%AE%E6%8F%92%E5%85%A5-%E6%9F%A5%E6%89%BE-%E5%88%A0%E9%99%A4-%E9%81%8D%E5%8E%86-%E6%BA%90%E7%A0%81%E5%88%86%E6%9E%90.html)
---
+++ interview-05/src/test/java/org/itstack/interview/test/ApiTest.java
@@ -0,0 +1,108 @@
+package org.itstack.interview.test;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ApiTest {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("", "");
+        map.get("");
+        map.remove("");
+
+
+    }
+
+    @Test
+    public void test_Iterator() {
+        Map<String, String> map = new HashMap<String, String>(64);
+        map.put("24", "Idx：2");
+        map.put("46", "Idx：2");
+        map.put("68", "Idx：2");
+        map.put("29", "Idx：7");
+        map.put("150", "Idx：12");
+        map.put("172", "Idx：12");
+        map.put("194", "Idx：12");
+        map.put("271", "Idx：12");
+
+        System.out.println("排序01：");
+        for (String key : map.keySet()) {
+            System.out.print(key + " ");
+        }
+
+        map.put("293", "Idx：12");
+        map.put("370", "Idx：12");
+        map.put("392", "Idx：12");
+        map.put("491", "Idx：12");
+        map.put("590", "Idx：12");
+
+        System.out.println("\n\n排序02：");
+        for (String key : map.keySet()) {
+            System.out.print(key + " ");
+        }                      
+
+        map.remove("293");
+        map.remove("370");
+        map.remove("392");
+        map.remove("491");
+        map.remove("590");
+
+        System.out.println("\n\n排序03：");
+        for (String key : map.keySet()) {
+            System.out.print(key + " ");
+        }
+        
+    }
+
+    @Test
+    public void test_idx12() {
+
+        for (int i = 1; i < 1000; i++) {
+            String key = String.valueOf(i);
+            int hash = key.hashCode() ^ (key.hashCode() >>> 16);
+            int idx = (64 - 1) & hash;
+
+            if (idx == 2) {
+                System.out.println(i + " Idx：" + idx);
+            }
+
+            if (idx == 7) {
+                System.out.println(i + " Idx：" + idx);
+            }
+
+            if (idx == 12) {
+                System.out.println(i + " Idx：" + idx);
+            }
+        }
+
+        /**
+         * idx：12
+         * 40
+         * 51
+         * 62
+         * 73
+         * 84
+         * 95
+         * 150
+         * 161
+         *
+         * idx：2
+         * 13
+         * 24
+         *
+         * idx：7
+         *
+         * 18
+         * 29
+         * 90
+         */
+
+    }
+
+}
